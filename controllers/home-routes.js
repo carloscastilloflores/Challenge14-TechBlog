ba5b133a3379be725c5c841cfb85d79feeb5d62e
carloscastilloflores <125534814+carloscastilloflores@users.noreply.github.com>
2023-07-05 12:39:51 -0600

    fixed logout routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -150,6 +150,14 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
 
 module.exports = router;
